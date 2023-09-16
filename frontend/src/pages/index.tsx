f6929ebe03b34324dd821439a5354b2e6931d572
MarsIfeanyi <marschalice@gmail.com>
2023-09-16 02:24:41 +0100

    Create Home Page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,3 +1,10 @@
+import dynamic from "next/dynamic";
+import GetBoardColors from "@/components/GetBoardColors";
+const NoSSRGET = dynamic(() => import("../components/GetBoardColors"), {
+  ssr: false,
+});
+import SetBoardColors from "@/components/SetBoardColors";
+
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import Head from "next/head";
 
@@ -9,10 +16,20 @@ export default function Home() {
       </Head>
 
       <main>
-        <div className="flex  justify-between  mx-14 font-bold mt-10 text-blue-600">
+        <div className="  mx-14 font-bold mt-6 text-blue-600 ">
           <h1>ColorBoard</h1>
+          <div className="flex justify-end">
+            <ConnectButton showBalance />
+          </div>
 
-          <ConnectButton />
+          <div className=" flex flex-row items-center justify-center gap-20">
+            <div>
+              <SetBoardColors />
+            </div>
+            <div>
+              <NoSSRGET />
+            </div>
+          </div>
         </div>
       </main>
     </>
