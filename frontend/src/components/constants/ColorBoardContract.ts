852ab57ebb9450c2511615b288564bf0a5b12bbd
MarsIfeanyi <marschalice@gmail.com>
2023-09-16 02:24:19 +0100

    Create Constant file for ABI and ContractAddress

---
+++ frontend/src/components/constants/ColorBoardContract.ts
@@ -0,0 +1,6 @@
+import contractABI from "./ColorBoard.json";
+
+export const CONTRACT_ADDRESS: string =
+  "0xC1BA6C7bDa02477c8E42F0aCb037b78b60302103";
+
+export const CONTRACT_ABI: any = contractABI.abi;
